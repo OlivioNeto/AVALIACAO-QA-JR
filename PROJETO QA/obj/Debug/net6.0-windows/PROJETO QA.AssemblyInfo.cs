95779f5d27f5fb9f3b895eba139bb3562aaef225
Olivio Neto <netoolivio34@gmail.com>
2026-06-22 08:07:35 -0300

    Subindo alterações realizadas para não perder as mesmas na máquina

--- PROJETO QA/obj/Debug/net6.0-windows/PROJETO QA.AssemblyInfo.cs
+++ PROJETO QA/obj/Debug/net6.0-windows/PROJETO QA.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PROJETO QA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5240ad500d2923987ae92942e40ccb28480f0d86")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5890450a98c5c2b1526d5d16d9f2d1f2870067c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PROJETO QA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PROJETO QA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
